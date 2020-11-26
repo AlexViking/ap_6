77ea6ffc7312682768057164fdbbc3d50ec8f15c
Aleksandre Khundzakishvili <3ds.alex@gmail.com>
2020-11-26 21:44:22 +0200

    added login / register components

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Header from "./components/header";
 import Sidebar from "./components/sidebar";
 import Footer from "./components/footer";
+import Register from "./components/register";
+import Login from "./components/login";
 import Dashboard from "./components/dashboard";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -13,10 +15,12 @@ export default class App extends Component {
       <Router>
         <Switch>
           <div>
-            <Header />
-            <Sidebar />
+            {false && <Header />}
+            {false && <Sidebar />}
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
             <Route path="/dashboard" component={Dashboard} />
-            <Footer />
+            {false && <Footer />}
           </div>
         </Switch>
       </Router>
